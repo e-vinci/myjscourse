755bb34944584d012acd2188addfa36be9087f72
e-baron <raphael.baroni@vinci.be>
2021-08-04 08:49:10 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9095e7d25a50c3508047.js"
+    "url": "webpack-runtime-8246c89f3b9872ac9075.js"
   },
   {
     "url": "framework-3dd7546f04adb66c5541.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "58d9ed3b2f6d1a9616149bfe179cd2ca"
+    "revision": "5fb693c0515c605a72d02ba7c6faf8e0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a6b81a7058608ac82586.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "6eedd0811c42d3130a07837f71c755bb"
+    "revision": "9989871af89a3fb696c3c0bf955fbd91"
   },
   {
     "url": "polyfill-83d6d241e8130eacdbf1.js"
